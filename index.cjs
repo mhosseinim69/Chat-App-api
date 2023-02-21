1134b60ac83e2568519de8b273af809507542610
Mostafa Hosseini <m.hosseini.m69@gmail.com>
2023-02-21 17:41:13 +0330

    addapi

--- index.cjs
+++ index.cjs
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "https://localhost:3000",
+    origin: "https://chat-app-9pd5.onrender.com",
     Credentials: true,
   },
 });
